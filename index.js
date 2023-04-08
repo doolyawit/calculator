cb6d0b5e4b5e0a823f49f61864a84286af34165c
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-04-08 20:21:41 +0700

    perfect

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 let num1 = 8;
 let num2 = 2;
 let sumel = document.getElementById("sum-el");
-let sum =0;
+let sum = 0;
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
 
-function add(){
-    sum = num1+num2;
-    sumel.textContent=sum
+function add() {
+  sum = num1 + num2;
+  sumel.textContent = "Sum : " + sum;
 }
-function subtract(){
-    sum = num1-num2;
-    sumel.textContent=sum
+function subtract() {
+  sum = num1 - num2;
+  sumel.textContent = "Sum : " + sum;
 }
-function divide(){
-    sum = num1/num2;
-    sumel.textContent=sum
+function divide() {
+  sum = num1 / num2;
+  sumel.textContent = "Sum : " + sum;
 }
-function multiply(){
-    sum = num1*num2;
-    sumel.textContent=sum
+function multiply() {
+  sum = num1 * num2;
+  sumel.textContent = "Sum : " + sum;
 }
-
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
@@ -30,6 +29,3 @@ function multiply(){
 
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
-
-
-
